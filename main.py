29534402f06980dc5d91d8a163cc9b97c2a0f0e9
Clément Palézis <clement.palezis@denergium.com>
2025-10-23 23:55:33 +0200

    fix(api/main.py): add frontend url into cors config

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8002", "http://localhost:5173", "http://localhost:8080",
-                "http://172.19.0.3:8002", "http://172.19.0.3:5173", "http://172.19.0.3:8080"],
+                "http://172.19.0.3:8002", "http://172.19.0.3:5173", "http://172.19.0.3:8080",
+                "https://tgv-transit-tally.vercel.app/"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
